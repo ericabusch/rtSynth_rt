55e6366327383e787b0f4919f53b9cbb25632eb6
KailongPeng <kailongpeng001@gmail.com>
2021-01-03 13:01:10 -0500

    prepare folder structure

--- rtCommon/cfg_loading.py
+++ rtCommon/cfg_loading.py
@@ -1,7 +1,6 @@
-
 import rtCommon.utils as utils
 import os
-
+# sys.path.append('/gpfs/milgram/project/turk-browne/users/kp578/realtime/rt-cloud/')
 def mkdir(folder):
     if not os.path.isdir(folder):
         os.mkdir(folder)
@@ -10,9 +9,19 @@ def cfg_loading(toml=''):
     # toml="pilot_sub001.ses1.toml"
     cfg = utils.loadConfigFile(f"/gpfs/milgram/project/turk-browne/users/kp578/realtime/rt-cloud/projects/rtSynth_rt/conf/{toml}")
 
-    cfg.dicom_dir=f"{cfg.dicom_folder}{cfg.YYYYMMDD}.{cfg.LASTNAME}/"
-    cfg.recognition_dataFolder=f"{cfg.subjects_dir}/{cfg.subjectName}/{cfg.session}_recognition/"
-    cfg.feedback_dataFolder=f"{cfg.subjects_dir}/{cfg.subjectName}/{cfg.session}_feedback/"
-    cfg.model_dir=f"{cfg.subjects_dir}/{cfg.subjectName}/{cfg.session}_recognition/clf/"
-    
+    cfg.dicom_dir         = f"{cfg.dicom_folder}{cfg.YYYYMMDD}.{cfg.LASTNAME}/"
+    cfg.recognition_dir   = f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session}/recognition/"
+    cfg.feedback_dir      = f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session}/feedback/"
+    cfg.usingModel_dir    = f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session-1}/recognition/clf/"
+    cfg.trainingModel_dir = f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session}/recognition/clf/"
+
+    # prepare folder structure
+    for curr_ses in range(1,6):
+        mkdir(f"subjects/{cfg.subjectName}/ses{curr_ses}/catPer/")
+        mkdir(f"subjects/{cfg.subjectName}/ses{curr_ses}/anat/")
+        mkdir(f"subjects/{cfg.subjectName}/ses{curr_ses}/recognition/")
+        mkdir(f"subjects/{cfg.subjectName}/ses{curr_ses}/recognition/clf/")
+        mkdir(f"subjects/{cfg.subjectName}/ses{curr_ses}/feedback/")
+
     return cfg
+
