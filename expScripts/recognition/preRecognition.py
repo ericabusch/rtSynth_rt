4966510678fed160d32c3047988dfe9130567941
KailongPeng <kailongpeng001@gmail.com>
2021-01-10 16:18:10 -0500

    expScripts/recognition/preRecognition.py print number of max TR before the recognition run given input curr_run and cfg

---
+++ expScripts/recognition/preRecognition.py
@@ -0,0 +1,29 @@
+# preRecognition.py aims to print out the number of TRs for the current recognition run.
+
+'''
+input: 
+    cfg
+    curr_run
+'''
+
+import os
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+sys.path.append('/gpfs/milgram/project/turk-browne/projects/rtSynth_rt/')
+import rtCommon.utils as utils
+from rtCommon.utils import loadConfigFile
+argParser = argparse.ArgumentParser()
+argParser.add_argument('--config', '-c', default='pilot_sub001.ses1.toml', type=str, help='experiment file (.json or .toml)')
+args = argParser.parse_args()
+from rtCommon.cfg_loading import mkdir,cfg_loading
+cfg = cfg_loading(args.config)
+
+curr_run=int(sys.argv[0])
+
+choose = np.load(f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session}/recognition/choose.npy")
+order = f'{cfg.orderFolder}/recognitionOrders_{choose[curr_run - 1]}.csv'
+trial_list = pd.read_csv(order)
+maxTR=trial_list['time'].iloc[-1]/cfg.TR
+print(f"maxTR={maxTR}")
