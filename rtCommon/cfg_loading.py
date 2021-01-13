9765bbd16c30995118ee33cc0012cd6b8c1a0da7
KailongPeng <kailongpeng001@gmail.com>
2021-01-13 03:24:23 -0500

    add cfg.tmp_folder

--- rtCommon/cfg_loading.py
+++ rtCommon/cfg_loading.py
@@ -9,7 +9,7 @@ def cfg_loading(toml=''):
     def findDir(path):
         from glob import glob
         return glob(path)[0]+'/'
-        
+
     # toml="pilot_sub001.ses1.toml"
     # cfg = utils.loadConfigFile(f"/gpfs/milgram/project/turk-browne/users/kp578/realtime/rt-cloud/projects/rtSynth_rt/conf/{toml}")
     cfg = utils.loadConfigFile(f"/gpfs/milgram/project/turk-browne/projects/rtSynth_rt/projects/rtSynth_rt/conf/{toml}")
@@ -25,7 +25,7 @@ def cfg_loading(toml=''):
         raise Exception('path error')
 
     
-
+    cfg.tmp_folder="/gpfs/milgram/scratch60/turk-browne/kp578/rtcloud_rt/"
     cfg.orderFolder=f'{cfg.projectDir}expScripts/recognition/orders/'
     cfg.subjects_dir=f'{cfg.projectDir}subjects/'
     cfg.dicom_folder="/gpfs/milgram/project/realtime/DICOM/"
@@ -34,25 +34,25 @@ def cfg_loading(toml=''):
 
     cfg.preDay_dicom_dir  = findDir(f"{cfg.dicom_folder}{cfg.preDay_YYYYMMDD}.{cfg.LASTNAME}*.{cfg.LASTNAME}*/")  #e.g. /gpfs/milgram/project/realtime/DICOM/20201009.rtSynth_pilot001.rtSynth_pilot001/  # cfg.preDay_YYYYMMDD is "0" when there is no previous day
     cfg.dicom_dir         = findDir(f"{cfg.dicom_folder}{cfg.YYYYMMDD}.{cfg.LASTNAME}*.{cfg.LASTNAME}*/")  #e.g. /gpfs/milgram/project/realtime/DICOM/20201019.rtSynth_pilot001_2.rtSynth_pilot001_2/ inside which is like 001_000003_000067.dcm
-    cfg.recognition_dir   = f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session}/recognition/"
-    cfg.feedback_dir      = f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session}/feedback/"
-    cfg.usingModel_dir    = f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session-1}/recognition/clf/"
-    cfg.trainingModel_dir = f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session}/recognition/clf/"
+    cfg.recognition_dir   = f"{cfg.subjects_dir}{cfg.subjectName}/ses{cfg.session}/recognition/"
+    cfg.feedback_dir      = f"{cfg.subjects_dir}{cfg.subjectName}/ses{cfg.session}/feedback/"
+    cfg.usingModel_dir    = f"{cfg.subjects_dir}{cfg.subjectName}/ses{cfg.session-1}/recognition/clf/"
+    cfg.trainingModel_dir = f"{cfg.subjects_dir}{cfg.subjectName}/ses{cfg.session}/recognition/clf/"
 
     # prepare folder structure
     for curr_ses in [1,5]:
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/")
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/catPer/")
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/anat/")
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/recognition/")
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/recognition/clf/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/catPer/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/anat/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/recognition/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/recognition/clf/")
     
 
     for curr_ses in [2,3,4]:
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/")
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/recognition/")
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/recognition/clf/")
-        mkdir(f"{cfg.projectDir}subjects/{cfg.subjectName}/ses{curr_ses}/feedback/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/recognition/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/recognition/clf/")
+        mkdir(f"{cfg.subjects_dir}{cfg.subjectName}/ses{curr_ses}/feedback/")
 
 
     return cfg
