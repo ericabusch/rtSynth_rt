beb923bd36be9e6c4daab1abfcfd73e7fb9b7683
KailongPeng <kailongpeng001@gmail.com>
2021-01-14 10:04:01 -0500

    print all maxTRs for the current session

--- expScripts/recognition/preRecognition.py
+++ expScripts/recognition/preRecognition.py
@@ -17,7 +17,13 @@ import sys
 import argparse
 import numpy as np
 import pandas as pd
-sys.path.append('/gpfs/milgram/project/turk-browne/projects/rtSynth_rt/')
+if 'watts' in os.getcwd():
+    main_dir = "/home/watts/Desktop/ntblab/kailong/rtcloud_rt/" # main_dir = "/home/watts/Desktop/ntblab/kailong/rtcloud_kp/"
+else:
+    main_dir="/Users/kailong/Desktop/rtEnv/rtSynth_rt/"
+
+sys.path.append(main_dir)
+
 import rtCommon.utils as utils
 from rtCommon.utils import loadConfigFile
 argParser = argparse.ArgumentParser()
@@ -30,7 +36,8 @@ cfg = cfg_loading(args.config)
 curr_run=int(args.run)
 
 choose = np.load(f"{cfg.subjects_dir}/{cfg.subjectName}/ses{cfg.session}/recognition/choose.npy")
-order = f'{cfg.orderFolder}/recognitionOrders_{choose[curr_run - 1]}.csv'
-trial_list = pd.read_csv(order)
-maxTR=trial_list['time'].iloc[-1]/cfg.TR
-print(f"maxTR={maxTR}")
+for curr_run in range(1,1+len(choose)):
+    order = f'{cfg.orderFolder}/recognitionOrders_{choose[curr_run - 1]}.csv'
+    trial_list = pd.read_csv(order)
+    maxTR=trial_list['time'].iloc[-1]/cfg.TR
+    print(f"curr_run={curr_run} maxTR={maxTR}")
