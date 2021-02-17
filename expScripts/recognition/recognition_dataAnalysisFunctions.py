2cfebbcbd507dc9e7d09745d143c352758beaa87
KailongPeng <kailongpeng001@gmail.com>
2021-02-17 13:43:11 -0500

    add subtract 0 within TR normalization

--- expScripts/recognition/recognition_dataAnalysisFunctions.py
+++ expScripts/recognition/recognition_dataAnalysisFunctions.py
@@ -248,6 +248,7 @@ def minimalClass(cfg):
         t = np.load(f"{cfg.recognition_dir}brain_run{run}.npy")
         mask = np.load(f"{cfg.chosenMask}")
         t = t[:,mask==1]
+        t = normalize(t)
         brain_data=t if ii==0 else np.concatenate((brain_data,t), axis=0)
 
         t = pd.read_csv(f"{cfg.recognition_dir}behav_run{run}.csv")
@@ -257,6 +258,7 @@ def minimalClass(cfg):
         t = np.load(f"{cfg.subjects_dir}{cfg.subjectName}/ses{cfg.session-1}/recognition/brain_run{run}.npy")
         mask = np.load(f"{cfg.chosenMask}")
         t = t[:,mask==1]
+        t = normalize(t)
         brain_data = np.concatenate((brain_data,t), axis=0)
 
         t = pd.read_csv(f"{cfg.subjects_dir}{cfg.subjectName}/ses{cfg.session-1}/recognition/behav_run{run}.csv")
@@ -287,9 +289,6 @@ def minimalClass(cfg):
 
     # Decide on the proportion of crescent data to use for classification
     include = 1
-    accuracyContainer=[]
-
-
     allpairs = itertools.combinations(objects,2)
 
     # Iterate over all the possible target pairs of objects
@@ -719,10 +718,6 @@ def greedyMask(cfg,N=26): # N used to be 31
     config="sub001.ses1.toml"
     cfg = cfg_loading(config)
 
-
-
-
-
     subject,dataSource,roiloc,N=cfg.subjectName,"realtime","schaefer2018",N
     # subject,dataSource,roiloc,N=sys.argv[1],sys.argv[2],sys.argv[3],int(sys.argv[4])
 
@@ -751,51 +746,6 @@ def greedyMask(cfg,N=26): # N used to be 31
 
 
     workingDir="/gpfs/milgram/project/turk-browne/projects/rtSynth_rt/subjects/sub001/ses1/recognition"
-    # workingDir="/gpfs/milgram/project/turk-browne/projects/rtTest/"
-    # subjects_correctly_aligned=['1206161','0119173','1206162','1130161','1206163','0120171','0111171','1202161','0125172','0110172','0123173','0120173','0110171','0119172','0124171','0123171','1203161','0118172','0118171','0112171','1207162','0117171','0119174','0112173','0112172']
-    # if roiloc == "schaefer2018":
-    #     RESULT=np.empty((len(subjects_correctly_aligned),300))
-    #     topN = []
-    #     for ii,sub in enumerate(subjects_correctly_aligned):
-    #         outloc = workingDir+"/{}/{}/output".format(roiloc, sub)
-    #         for roinum in range(1,301):
-    #             result = np.load("{}/{}.npy".format(outloc, roinum))
-    #             RESULT[ii,roinum-1]=result
-    #             # RESULT = result if roinum == 1 else np.vstack((RESULT, result))
-    #     RESULT = np.mean(RESULT,axis=0)
-    #     print(f"RESULT.shape={RESULT.shape}")
-    #     RESULTix = RESULT[:].argsort()[-N:]
-    #     for idx in RESULTix:
-    #         topN.append("{}.nii.gz".format(idx+1))
-    #         # print(topN[-1])
-    # else:
-    #     RESULT_all=[]
-    #     topN = []
-    #     for ii,sub in enumerate(subjects_correctly_aligned):
-    #         outloc = workingDir+"/{}/{}/output".format(roiloc, sub)
-    #         for hemi in ["lh", "rh"]:
-    #             for roinum in range(1, 26):
-    #                 result = np.load("{}/roi{}_{}.npy".format(outloc, roinum, hemi))
-    #                 Result = result if roinum == 1 else np.vstack((Result, result))
-    #             RESULT = Result if hemi == "lh" else np.hstack((RESULT, Result))
-    #         RESULT_all.append(RESULT)
-
-    #     RESULT_all=np.asarray(RESULT_all)
-    #     print(f"RESULT_all.shape={RESULT_all.shape}")
-    #     RESULT_all=np.mean(RESULT_all,axis=0)
-    #     print(f"RESULT_all.shape={RESULT_all.shape}")
-    #     RESULT1d = RESULT.flatten()
-    #     RESULTix = RESULT1d.argsort()[-N:]
-    #     x_idx, y_idx = np.unravel_index(RESULTix, RESULT.shape)
-
-    #     # Check that we got the largest values.
-    #     for x, y, in zip(x_idx, y_idx):
-    #         print(x,y)
-    #         if y == 0:
-    #             topN.append("roi{}_lh.nii.gz".format(x+1))
-    #         else:
-    #             topN.append("roi{}_rh.nii.gz".format(x+1))
-    #         # print(topN[-1])
 
     topN = load_obj(f"{cfg.recognition_expScripts_dir}top{N}ROIs")
     print(f"len(topN)={len(topN)}")
@@ -810,8 +760,6 @@ def greedyMask(cfg,N=26): # N used to be 31
         X = X - X.mean(3)
         return X
 
-
-    # phasedict = dict(zip([1,2,3,4,5,6，7，8],["12", "12", "34", "34", "56", "56"]))
     imcodeDict={"A": "bed", "B": "Chair", "C": "table", "D": "bench"}
 
     def getMask(topN, cfg):
@@ -848,6 +796,7 @@ def greedyMask(cfg,N=26): # N used to be 31
     behav_data=[]
     for ii,run in enumerate(actualRuns): # load behavior and brain data for current session
         t = np.load(f"{cfg.recognition_dir}brain_run{run}.npy")
+        t = normalize(t)
         brain_data.append(t)
 
         t = pd.read_csv(f"{cfg.recognition_dir}behav_run{run}.csv")
@@ -856,6 +805,7 @@ def greedyMask(cfg,N=26): # N used to be 31
 
     for ii,run in enumerate(actualRuns_preDay): # load behavior and brain data for previous session
         t = np.load(f"{cfg.subjects_dir}{cfg.subjectName}/ses{cfg.session-1}/recognition/brain_run{run}.npy")
+        t = normalize(t)
         brain_data.append(t)
 
         t = pd.read_csv(f"{cfg.subjects_dir}{cfg.subjectName}/ses{cfg.session-1}/recognition/behav_run{run}.csv")
@@ -863,72 +813,6 @@ def greedyMask(cfg,N=26): # N used to be 31
         behav_data.append(t)
 
     save_obj([brain_data,behav_data],f"{cfg.projectDir}tmp_folder/{subject}_{dataSource}_{roiloc}_{N}") #{len(topN)}_{i}
-    # bcvar = [behav_data]
-    # runs = brain_data
-    # save_obj([bcvar,runs],f"./tmp_folder/{subject}_{dataSource}_{roiloc}_{N}") #{len(topN)}_{i}
-
-    # # Compile preprocessed data and corresponding indices
-    # metas = []
-    # for run in range(1, 8):
-    #     print(run, end='--')
-    #     # retrieve from the dictionary which phase it is, assign the session
-    #     phase = phasedict[run]
-    #     ses = 1
-        
-    #     # Build the path for the preprocessed functional data
-    #     this4d = funcdata.format(ses=ses, run=run, phase=phase, sub=subject)
-        
-    #     # Read in the metadata, and reduce it to only the TR values from this run, add to a list
-    #     thismeta = pd.read_csv(metadata.format(ses=ses, run=run, phase=phase, sub=subject))
-    #     if dataSource == "neurosketch":
-    #         _run = 1 if run % 2 == 0 else 2
-    #     else:
-    #         _run = run
-    #     thismeta = thismeta[thismeta['run_num'] == int(_run)]
-        
-    #     if dataSource == "realtime":
-    #         TR_num = list(thismeta.TR.astype(int))
-    #         labels = list(thismeta.Item)
-    #         labels = [imcodeDict[label] for label in labels]
-    #     else:
-    #         TR_num = list(thismeta.TR_num.astype(int))
-    #         labels = list(thismeta.label)
-        
-    #     print("LENGTH OF TR: {}".format(len(TR_num)))
-    #     # Load the functional data
-    #     runIm = nib.load(this4d)
-    #     affine_mat = runIm.affine
-    #     runImDat = runIm.get_data()
-        
-    #     # Use the TR numbers to select the correct features
-    #     features = [runImDat[:,:,:,n+3] for n in TR_num]
-    #     features = np.array(features)
-    #     # features = features[:, mask==1]
-    #     print("shape of features", features.shape, "shape of mask", mask.shape)
-    #     featmean = features.mean(1).mean(1).mean(1)[..., None,None,None] #features.mean(1)[..., None]
-    #     features = features - featmean
-    #     features = np.expand_dims(features, 0)
-        
-    #     # Append both so we can use it later
-    #     metas.append(labels)
-    #     runs = features if run == 1 else np.concatenate((runs, features))
-
-    # dimsize = runIm.header.get_zooms()
-    # # Preset the variables
-    # print("Runs shape", runs.shape)
-    # bcvar = [metas]
-
-
-                    
-    # # Distribute the information to the searchlights (preparing it to run)
-    # _runs = [runs[:,:,mask==1]]
-    # print("Runs shape", _runs[0].shape)
-    # slstart = time.time()
-    # sl_result = Class(_runs, bcvar)
-    # print("results of classifier: {}, type: {}".format(sl_result, type(sl_result)))
-    # SL = time.time() - slstart
-    # tot = time.time() - starttime
-    # print('total time: {}, searchlight time: {}'.format(tot, SL))
 
     def wait(tmpFile):
         while not os.path.exists(tmpFile+'_result.npy'):
@@ -944,8 +828,6 @@ def greedyMask(cfg,N=26): # N used to be 31
         numberOfJobsRunning = int(open(f"{cfg.projectDir}squeue/{randomID}.txt", "r").read())
         print(f"numberOfJobsRunning={numberOfJobsRunning}")
         return numberOfJobsRunning
-
-
 
     def Class(brain_data,behav_data):
         # metas = bcvar[0]
