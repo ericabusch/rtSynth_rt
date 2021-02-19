1ef2ba2164cf248b7d18280be2485a904a569324
KailongPeng <kailongpeng001@gmail.com>
2021-02-19 17:37:02 -0500

    non verbose

--- projects/rtSynth_rt/rtSynth_rt.py
+++ projects/rtSynth_rt/rtSynth_rt.py
@@ -356,9 +356,9 @@ def doRuns(cfg, dataInterface, subjInterface, webInterface):
         _maskedData = normalize(maskedData)
 
         print(f"_maskedData.shape={_maskedData.shape}")
-        print(f"X.shape={X.shape}")
+        # print(f"X.shape={X.shape}")
         X = np.expand_dims(_maskedData[-1], axis=0)
-        print(f"X.shape={X.shape}")
+        # print(f"X.shape={X.shape}")
         # print(f"X={X}")
         
         Y = ['chair'] * X.shape[0]
