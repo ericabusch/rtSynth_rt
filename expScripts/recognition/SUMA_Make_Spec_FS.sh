7791e199106e7526592fb743ba33cc819b78ca78
KailongPeng <kailongpeng001@gmail.com>
2021-04-30 14:35:58 -0400

    SUMA_Make_Spec_FS.sh

---
+++ expScripts/recognition/SUMA_Make_Spec_FS.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Input python command to be submitted as a job
+#SBATCH --output=logs/SUMA_Make_Spec_FS-%j.out
+#SBATCH --job-name SUMA_Make_Spec_FS
+#SBATCH --partition=short,scavenge,day
+#SBATCH --time=2:00:00
+#SBATCH --mem=10000
+module load AFNI
+module load FreeSurfer/6.0.0
+# echo $FREESURFER_HOME # /gpfs/milgram/apps/hpc.rhel7/software/FreeSurfer/6.0.0
+
+subject=$1
+anatPath=/gpfs/milgram/project/turk-browne/projects/rtSynth_rt/subjects/${subject}/ses1/anat/
+subjectFolder=${anatPath}freesurfer/${subject}/
+
+cd ${subjectFolder}
+@SUMA_Make_Spec_FS -sid ${subject}
