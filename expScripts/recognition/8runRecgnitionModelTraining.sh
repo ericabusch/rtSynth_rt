dff18a07f2b495b187d4cf55325b8aa8ae0dcd3a
KailongPeng <kailongpeng001@gmail.com>
2021-06-06 20:54:15 -0400

    forceGreedy

--- expScripts/recognition/8runRecgnitionModelTraining.sh
+++ expScripts/recognition/8runRecgnitionModelTraining.sh
@@ -11,7 +11,14 @@ module load FSL
 source activate /gpfs/milgram/project/turk-browne/users/kp578/CONDA/rtcloud
 
 
-toml=$1
-scan_asTemplate=$2
+toml=$1 # sub002.ses5.toml
+scan_asTemplate=$2 # 1
+forceGreedy=$3 # 1 #skipPre=$3 # 1
 
-python -u /gpfs/milgram/project/turk-browne/projects/rtSynth_rt/expScripts/recognition/8runRecgnitionModelTraining.py -c $toml --scan_asTemplate $scan_asTemplate
+if [ "$forceGreedy" == "1" ]; then
+    echo "forceGreedy"
+    python -u /gpfs/milgram/project/turk-browne/projects/rtSynth_rt/expScripts/recognition/8runRecgnitionModelTraining.py -c $toml --scan_asTemplate $scan_asTemplate --skipPre --forceGreedy
+else
+    echo "not forceGreedy"
+    python -u /gpfs/milgram/project/turk-browne/projects/rtSynth_rt/expScripts/recognition/8runRecgnitionModelTraining.py -c $toml --scan_asTemplate $scan_asTemplate
+fi
